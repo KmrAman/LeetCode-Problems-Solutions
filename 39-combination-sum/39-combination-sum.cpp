d33637c4bd63f839942275312d742e5d18d8e3a7
KmrAman <65306581+KmrAman@users.noreply.github.com>
2022-09-27 01:11:13 +0530

    Time: 6 ms (89.58%), Space: 10.6 MB (98.98%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -3,23 +3,24 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> res;
         vector<int> path;
-        sort(candidates.begin(), candidates.end());
-        backtracking(candidates, target, 0, path, res);
+        backtracking(0,target,candidates,path,res);
         return res;
     }
 private:
-    void backtracking(vector<int>& candidates, int target, int pos, vector<int> &path, vector<vector<int>> &res) {
-        if (target == 0) {
+    void backtracking(int ind,int target,vector<int>& nums,vector<int> &path, vector<vector<int>> &res) {
+        if(ind==nums.size()){
+            if (target == 0) {
             res.push_back(path);
+            }
             return;
         }
-        for (int i = pos; i < candidates.size(); i++) {
-            if (candidates[i] > target) {
-                break;
-            }
-            path.push_back(candidates[i]);
-            backtracking(candidates, target - candidates[i], i, path, res);
+        
+        if(nums[ind]<=target){
+            path.push_back(nums[ind]);
+            backtracking(ind,target-nums[ind],nums,path,res);
             path.pop_back();
         }
+        backtracking(ind+1,target,nums,path,res);
+    
     }
 };
