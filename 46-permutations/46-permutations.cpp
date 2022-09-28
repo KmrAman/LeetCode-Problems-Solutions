680e54d33fe1bc3e88b88f3d240f8523c4d9d3dc
KmrAman <65306581+KmrAman@users.noreply.github.com>
2022-09-28 12:13:33 +0530

    Time: 8 ms (32.93%), Space: 7.7 MB (55.42%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,25 +1,20 @@
 class Solution {
-public:
-    void generate(int ind,vector<int> &nums,vector<bool> &freq,vector<vector<int>> &res,vector<int> &ans){
-        if(ans.size()==nums.size()){
-            res.push_back(ans);
+public: 
+    void generate(int ind,vector<int> &nums,vector<vector<int>> &res){
+        if(ind==nums.size()){
+            res.push_back(nums);
                 return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                ans.push_back(nums[i]);
-                freq[i]=true;
-                generate(i+1,nums,freq,res,ans);
-                ans.pop_back();
-                freq[i]=false;
-            }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            generate(ind+1,nums,res);
+            swap(nums[ind],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> ans;
-        vector<bool> freq(nums.size(),false);
-        generate(0,nums,freq,res,ans);
+        generate(0,nums,res);
         return res;
     }
 };
