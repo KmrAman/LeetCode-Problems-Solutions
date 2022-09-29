41e3cba6b296b16043df506039cac6f59016ce98
KmrAman <65306581+KmrAman@users.noreply.github.com>
2022-09-29 10:08:27 +0530

    Time: 3 ms (87.82%), Space: 6.3 MB (47.45%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,35 +1,36 @@
 class Solution {
   public:
-    void solve(int col,vector<string> &board, vector<vector<string>> &ans,vector<int> & leftrow,vector<int> &upperDiagonal,vector<int> &lowerDiagonal,int n) {
+    int solve(int col,vector<string> &board,vector<int> & leftrow,vector<int> &upperDiagonal,vector<int> &lowerDiagonal,int n) {
       if (col == n) {
-        ans.push_back(board);
-        return;
+        return 1;
       }
+        int count=0;
       for (int row = 0; row < n; row++) {
         if (leftrow[row]==0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
           board[row][col] = 'Q';
           leftrow[row] = 1;
           lowerDiagonal[row + col] = 1;
           upperDiagonal[n - 1 + col - row] = 1;
-          solve(col + 1, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
+          count+=solve(col + 1, board, leftrow, upperDiagonal, lowerDiagonal, n);
           board[row][col] = '.';
           leftrow[row] = 0;
           lowerDiagonal[row + col] = 0;
           upperDiagonal[n - 1 + col - row] = 0;
         }
       }
+        return count;
     }
 
   public:
      int totalNQueens(int n) {
-      vector<vector<string>> ans;
+      // vector<vector<string>> ans;
       vector<string> board(n);
       string s(n, '.');
       for (int i=0;i<n;i++) {
         board[i] = s;
       }
       vector<int> leftrow(n, 0),upperDiagonal(2 * n - 1, 0),lowerDiagonal(2 * n - 1, 0);
-      solve(0, board,ans,leftrow,upperDiagonal,lowerDiagonal,n);
-      return ans.size();
+      return solve(0, board,leftrow,upperDiagonal,lowerDiagonal,n);
+      
     }
 };
