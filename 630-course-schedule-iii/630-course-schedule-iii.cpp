7c9ad8cbcbf743835551475b2c9fcfb604f2e2ad
KmrAman <65306581+KmrAman@users.noreply.github.com>
2022-10-07 17:53:59 +0530

    Time: 1564 ms (12.86%), Space: 265.2 MB (12.08%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    static bool cmp(vector<int> a, vector<int> b) {
+        return a[1]<b[1];
+    }
+    int scheduleCourse(vector<vector<int>>& c) {
+        sort(c.begin(),c.end(),cmp);
+        priority_queue<int> pq;
+        int time=0;
+        for(int i=0;i<c.size();i++){
+            if(time+c[i][0]<=c[i][1]){
+                time+=c[i][0];
+                pq.push(c[i][0]);
+            }
+            else{
+                if(!pq.empty() && pq.top()>c[i][0]){
+                    time-=pq.top();
+                    pq.pop();
+                    time+=c[i][0];
+                    pq.push(c[i][0]);
+                }
+            }
+        }
+        return pq.size();
+    }
+};
