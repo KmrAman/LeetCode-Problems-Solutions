5c45d10da893845cfa17c5db4522b105d7be3536
KmrAman <65306581+KmrAman@users.noreply.github.com>
2022-10-18 22:48:18 +0530

    Time: 63 ms (88.79%), Space: 17.3 MB (81.10%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        priority_queue<int> pq;
-        unordered_map<char,int> mp;
-        unordered_map<int,int> mp1;
-        for(auto &x:s){
-            mp[x]++;
-        }
-        for(auto &x:mp){
-            pq.push(x.second);
-        }
-        mp1[pq.top()]++;
-        pq.pop();
         int ans=0;
-        while(!pq.empty()){
-            int temp=pq.top();
-            while(mp1[temp]>0 && temp!=0){
-                temp--;
-                ans++;
-            }
-            mp1[temp]++;
-            pq.pop();
+        vector<int> cnt(26,0);
+        for(auto &x:s){
+            cnt[x-'a']++;
+        }
+        sort(cnt.begin(),cnt.end());
+        for(int i=24;i>=0 && cnt[i]>0;--i){
+            ans+=max(0,cnt[i]-max(0,cnt[i+1]-1));
+            cnt[i]=min(cnt[i],max(0,cnt[i+1]-1));
         }
         return ans;
     }
